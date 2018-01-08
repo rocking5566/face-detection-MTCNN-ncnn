cc9a32cef71aa07460e87a9204465826088cd9de
davidsun <sunhm321@gmail.com>
2018-01-08 11:12:22 +0800

    Add mapping of eRGB888 to ncnn pixel format.

--- Mtcnn.cpp
+++ Mtcnn.cpp
@@ -21,10 +21,11 @@ int GetNcnnImageConvertType(imageType type)
 {
     switch (type)
     {
+	case eRGB888:
+        return ncnn::Mat::PIXEL_RGB;
     case eBGR888:
     default:
         return ncnn::Mat::PIXEL_BGR2RGB;
-
     }
 }
 
